c755344fb5fcd68e3ac4bc1837da097d155ceb02
Jefferson Payano <76220445+Jeffersonp4@users.noreply.github.com>
2021-09-01 10:52:20 -0400

    ignore

--- docker-django-postgres-nginx-master/docker_django/hola_docker/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class HolaDockerConfig(AppConfig):
-    name = 'hola_docker'
--- docker-django-postgres-nginx-master/docker_django/hola_docker/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from hola_docker import views
-
-urlpatterns = [
-    path('hola/', views.hola, name='hola'),
-]
